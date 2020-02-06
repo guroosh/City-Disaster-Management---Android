439d899b6a3d9d1451b86df601c7e74be4db0556
ShubhangiKukreti <55923146+ShubhangiKukreti@users.noreply.github.com>
2020-02-06 15:44:32 +0000

    Added registration API call

--- app/src/main/java/com/example/androidase_/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/androidase_/activities/RegistrationActivity.java
@@ -4,17 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androidase_.R;
+import com.example.androidase_.object_classes.CommonUser;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class RegistrationActivity extends AppCompatActivity {
     String _message = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +39,37 @@ public class RegistrationActivity extends AppCompatActivity {
         final TextView textViewMessage = findViewById(R.id.register_messageTextView);
 
         Button registerButton = findViewById(R.id.buttonRegister);
-        registerButton.setOnClickListener(new View.OnClickListener(){
+        registerButton.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v){
+            public void onClick(View v) {
                 _message = "";
-                boolean flg = true;
-                flg = checkCondition(R.id.editTextEmail,0,"Email");
-                flg = checkCondition(0,1,"The format of Email is incorrect.");
-                flg  = checkCondition(R.id.editTextFirstName,0,"First Name");
-                flg = checkCondition(R.id.editTextLastName,0,"Last Name");
-                flg = checkCondition(R.id.editTextGovernmentIDNumber,0,"Government ID Number");
-                flg = checkCondition(R.id.registration_editTextPassword,0,"Password");
-                flg = checkCondition(R.id.registration_editTextConfirmPassword,0,"Confirm Password");
-                flg = checkCondition(0,2,"Password does't match.");
+                boolean flg = (checkCondition(R.id.editTextEmail, 0, "Email")) &&
+                        (checkCondition(0, 1, "The format of Email is incorrect.")) &&
+                        (checkCondition(R.id.editTextFirstName, 0, "First Name")) &&
+                        (checkCondition(R.id.editTextLastName, 0, "Last Name")) &&
+                        (checkCondition(R.id.editTextGovernmentIDNumber, 0, "Government ID Number")) &&
+                        (checkCondition(R.id.registration_editTextPassword, 0, "Password")) &&
+                        (checkCondition(R.id.registration_editTextConfirmPassword, 0, "Confirm Password")) &&
+                        checkCondition(0, 2, "Password does't match.");
+                CheckBox checkBoxIsVolunteer = findViewById(R.id.checkBoxVolunteer);
+
+
+                //System.out.println(sin.isChecked());
                 textViewMessage.setText(_message);
-                if (flg)
-                {
-                    //send api with data
+                if (flg) {
+                    CommonUser commonUser = new CommonUser();
+                    commonUser.emailId = String.valueOf(R.id.editTextEmail);
+                    commonUser.password = String.valueOf(R.id.registration_editTextPassword);
+                    commonUser.phoneNumber = String.valueOf(R.id.editTextPhone);
+                    commonUser.governmentIdNumber = String.valueOf(R.id.editTextGovernmentIDNumber);
+                    commonUser.isVolunteering = checkBoxIsVolunteer.isChecked();
+                    commonUser.volunteeringField = String.valueOf(R.id.spinnerVolunteeringField);
+                    commonUser.governmentIdType = String.valueOf(R.id.spinnerGovernmentIDType);
+                    commonUser.setName(String.valueOf(R.id.editTextFirstName), String.valueOf(R.id.editTextLastName));
+                    createThreadPostToSignup("", commonUser.objToJson());
+                    Toast.makeText(getApplicationContext(), "yes", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "nope", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -59,34 +89,63 @@ public class RegistrationActivity extends AppCompatActivity {
 
     }
 
-    protected boolean checkCondition(int ID,int kind,String wrongMsg){
+    protected boolean checkCondition(int ID, int kind, String wrongMsg) {
         boolean flg = true;
         EditText editText;
-        switch (kind){
+        switch (kind) {
             case 0:
                 editText = findViewById(ID);
                 String text = editText.getText().toString();
-                if (text.isEmpty()){
-                    _message += wrongMsg+" is empty.\n";
+                if (text.isEmpty()) {
+                    _message += wrongMsg + " is empty.\n";
                     flg = false;
                 }
                 break;
             case 1:
                 editText = findViewById(R.id.editTextEmail);
-                if (editText.getText().toString().contains("@")){
-                    _message += wrongMsg+"\n";
+                if (editText.getText().toString().contains("@")) {
+                    _message += wrongMsg + "\n";
                     flg = false;
                 }
                 break;
             case 2:
                 EditText password1 = findViewById(R.id.registration_editTextPassword);
                 EditText password2 = findViewById(R.id.registration_editTextConfirmPassword);
-                if(!password1.getText().toString().equals(password2.getText().toString())){
-                    _message += wrongMsg+"\n";
+                if (!password1.getText().toString().equals(password2.getText().toString())) {
+                    _message += wrongMsg + "\n";
                     flg = false;
                 }
                 break;
         }
         return flg;
+    }
+
+    public void createThreadPostToSignup(final String url, final JSONObject object) throws NullPointerException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    postRestApi(url, object);
+                } finally {
+                    //empty
+                }
+            }
+        });
+        thread.start();
+    }
+
+    private void postRestApi(String url, JSONObject object) throws NullPointerException {
+        final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(object.toString(), JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/androidase_/object_classes/CommonUser.java
+++ app/src/main/java/com/example/androidase_/object_classes/CommonUser.java
@@ -1,5 +1,9 @@
 package com.example.androidase_.object_classes;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CommonUser {
     public Name name;
     public String emailId;
@@ -9,10 +13,42 @@ public class CommonUser {
     public String governmentIdNumber;
     public boolean isVolunteering;
     public String volunteeringField;
+
+    public JSONObject objToJson() {
+        JSONObject json = new JSONObject();
+        try {
+
+
+//Convert POJO to JSON
+
+            json.put("EmailId", this.emailId);
+            json.put("Password", this.password);
+            json.put("GovernmentIdNumber", this.governmentIdNumber);
+            json.put("PhoneNumber", this.phoneNumber);
+            json.put("IsVolunteering", this.isVolunteering);
+            json.put("GovernmentIdType", this.governmentIdType);
+            json.put("VolunteeringField", this.volunteeringField);
+            json.put("Name", this.getNameString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return json;
+    }
+
+    public void setName(String firstName, String lastName) {
+        this.name.firstName = firstName;
+        this.name.lastName = lastName;
+    }
+
+    public String getNameString() {
+        return this.name.firstName + " " + this.name.lastName;
+    }
 }
 
-class Name
-{
+class Name {
     public String firstName;
     public String lastName;
 }
