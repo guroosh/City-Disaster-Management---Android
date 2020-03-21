1595ee49df5c8c69ece420e333a30785d443cee2
littlethe <littlethe2000@yahoo.com.tw>
2020-03-21 21:39:43 +0000

    Add public mqtt example.

--- app/src/main/java/com/example/androidase_/activities/DBActivity.java
+++ app/src/main/java/com/example/androidase_/activities/DBActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidase_.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +13,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidase_.R;
 import com.example.androidase_.database.BaseDataHelper;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Base64.getEncoder;
+
 public class DBActivity extends AppCompatActivity {
+    private MqttAndroidClient client;
+    private String mqttTopic;
+    private TextView status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +83,97 @@ public class DBActivity extends AppCompatActivity {
                     s.append("\n");
                 }
                 textView.setText(s.toString());
+            }
+        });
+
+        status = findViewById(R.id.database_textView_status);
+        connectMQTT();
+        Button btnSend = findViewById(R.id.database_button_send);
+        btnSend.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText et = findViewById(R.id.database_editText_message);
+                String payload = et.getText().toString();
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(mqttTopic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void connectMQTT(){
+        mqttTopic = "ase/android";
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    status.setText("connection is successful.");
+                    subscribeMQTT();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    status.setText("connection is failed.");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+    private void subscribeMQTT(){
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(mqttTopic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                    status.setText("subscription is successful.");
+                    setMQTTCallback();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    status.setText("subscription is failed.");
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setMQTTCallback(){
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                status.setText("connection is lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                status.setText("message is received.");
+                String msg = new String(message.getPayload(), StandardCharsets.UTF_8);
+                TextView tv = findViewById(R.id.database_textView_chat);
+                tv.setText(msg);
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                status.setText("delivery completed.");
             }
         });
     }
